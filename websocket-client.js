d3ea3ee32409e6af30d71590187467269d945009
adahyto <adahy.to@gmail.com>
2020-03-21 09:20:47 +0100

    fix readme and lint

--- websocket-client.js
+++ websocket-client.js
@@ -1,24 +1,24 @@
 const WS = new WebSocket('ws://localhost:3000');
 
-WS.onmessage = (payload)=>{
+WS.onmessage = (payload) => {
     console.log(payload.data);
     displayMsgs(payload.data);
 };
-WS.onopen = ()=>{
+WS.onopen = () => {
     displayTitle('connection open');
 };
-WS.onclose = ()=>{
+WS.onclose = () => {
     displayTitle('connection closed');
 };
-function displayTitle(title){
+function displayTitle(title) {
     document.querySelector('h2').innerHTML = title;
 }
-function displayMsgs(message){
+function displayMsgs(message) {
     let p = document.createElement('p');
     p.innerHTML = message;
     document.querySelector('div.messages').appendChild(p);
 }
-document.forms[0].onsubmit = ()=>{
+document.forms[0].onsubmit = () => {
     let input = document.getElementById('message');
     console.log(input);
     WS.send(input.value);
--- websocket-server.js
+++ websocket-server.js
@@ -1,16 +1,16 @@
 const port = 3000;
 const WebSocketServer = require('ws').Server;
-const WSS = new WebSocketServer({port: port});
+const WSS = new WebSocketServer({ port: port });
 console.log(`You can connect on *:${port}`);
-WSS.on('connection', (ws)=>{
+WSS.on('connection', (ws) => {
     console.log("Connected");
-    ws.on('message', (message)=>{
+    ws.on('message', (message) => {
         console.log(message);
         if (message === 'close') {
             ws.close();
-        }else{
-            WSS.clients.forEach((client)=>{
-            client.send(message);
+        } else {
+            WSS.clients.forEach((client) => {
+                client.send(message);
             });
         }
     });
